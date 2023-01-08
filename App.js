b5741b471f4ed52812755761bb39304cb6ebfe99
KawshikBss <kawshikbss@gmail.com>
2023-01-08 20:28:37 +0600

    fixed navbar

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ShopScreen from './src/screens/ShopScreen';
+import SplashScreen from './src/screens/SplashScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        tabBarStyle: {...styles.navBar},
+      }}>
+        <Tab.Screen name='Splash' component={SplashScreen} />
+        <Tab.Screen name='Shop' component={ShopScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  navBar: {
+    borderTopLeftRadius:21, 
+    borderTopRightRadius:21,
+    backgroundColor:"#6c63ff",
+    position:'absolute',
+    bottom: 0,
+    padding:10,
+    width: '100%',
+    height: 54,
+    zIndex: 8 
+  }
 });
