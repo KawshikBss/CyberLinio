b7a487c6fa2c8d1e66061b59e1d712c6e2090949
KawshikBss <kawshikbss@gmail.com>
2023-01-09 23:22:56 +0600

    customized navbar

--- App.js
+++ App.js
@@ -1,35 +1,46 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ProductScreen from './src/screens/ProductScreen';
 import ShopScreen from './src/screens/ShopScreen';
 import SplashScreen from './src/screens/SplashScreen';
 
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const ShopTab = () => {
+      return (<Tab.Navigator screenOptions={{
+        tabBarStyle: {...styles.navBar},
+        headerShown: false,
+      }}>
+        <Tab.Screen name='Shop' component={ShopScreen} />
+      </Tab.Navigator>)
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-        tabBarStyle: {...styles.navBar},
-      }}>
-        <Tab.Screen name='Splash' component={SplashScreen} />
-        <Tab.Screen name='Shop' component={ShopScreen} />
-      </Tab.Navigator>
+  <Stack.Navigator screenOptions={{
+    headerShown: false,
+  }}>
+        <Stack.Screen name='Splash' component={SplashScreen} />
+        <Stack.Screen name='ShopTab' component={ShopTab} />
+        <Stack.Screen name='Product' component={ProductScreen} />
+  </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   navBar: {
-    borderTopLeftRadius:21, 
-    borderTopRightRadius:21,
-    backgroundColor:"#6c63ff",
+    borderTopLeftRadius:30, 
+    borderTopRightRadius:30,
     position:'absolute',
     bottom: 0,
-    padding:10,
     width: '100%',
-    height: 54,
-    zIndex: 8 
+    height: 60,
+    zIndex: 8,
   }
 });
