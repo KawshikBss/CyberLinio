c971ecad87e909162145832c18fd12e372d7d8cd
KawshikBss <kawshikbss@gmail.com>
2023-01-16 03:30:33 +0600

    switched to safe area view

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
 import React from 'react'
 
 const ProfileScreen = () => {
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <Text>ProfileScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+      height: "100%",
+      backgroundColor: "#8D72E1",
+      flexDirection: "column",
+  },
+});
 
 export default ProfileScreen
