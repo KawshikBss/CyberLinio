2a7fbb04f38ef4fa94086a9f53bd2d194deb966a
KawshikBss <kawshikbss@gmail.com>
2023-01-16 03:26:25 +0600

    reloccated card

---
+++ src/components/Card.js
@@ -0,0 +1,157 @@
+import { useNavigation } from "@react-navigation/native";
+import {
+    ImageBackground,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
+import Fontisto from "react-native-vector-icons/Fontisto";
+import AntDesign from "react-native-vector-icons/AntDesign";
+
+const Card = ({ product, shopView=false }) => {
+    const navigation = useNavigation();
+    return (
+        <View style={StyleSheet.flatten([styles.container, shopView? {width: 170, margin: 5}: {marginRight: 10}])}>
+            <TouchableOpacity
+                onPress={() =>
+                    navigation.navigate("Product", {
+                        screen: "Product",
+                        params: { product },
+                    })
+                }
+            >
+                <ImageBackground
+                    source={product.images[0]}
+                    style={styles.productImage}
+                    imageStyle={{
+                        borderRadius: 20,
+                        shadowColor: "#232323",
+                        shadowRadius: 10,
+                    }}
+                >
+                    <Text style={{...styles.tag, backgroundColor: product.new? '#8D72E1': styles.tag.backgroundColor, color: product.new? '#B9E0FF': styles.tag.color}}>
+                        {product?.tag ? product.tag : "-20%"}
+                    </Text>
+                    <TouchableOpacity
+                        style={styles.cartBtn}
+                        onPress={(e) => {
+                            e.stopPropagation();
+                        }}
+                    >
+                        <Fontisto
+                            style={styles.cartIcon}
+                            name="shopping-basket-add"
+                        />
+                    </TouchableOpacity>
+                </ImageBackground>
+            </TouchableOpacity>
+            <View style={styles.wrapper}>
+                <View style={styles.header}>
+                    <TouchableOpacity
+                        onPress={() =>
+                            navigation.navigate("Product", {
+                                screen: "Product",
+                                params: { product },
+                            })
+                        }
+                    >
+                        <Text style={styles.title}>
+                            {product.title
+                                ? product.title.length > 10
+                                    ? `${product.title.slice(0, 10)}...`
+                                    : product.title
+                                : "Product Title"}
+                        </Text>
+                    </TouchableOpacity>
+                    <View style={styles.rating}>
+                        <Text style={styles.ratingText}>
+                            {product.rating ? product.rating : "0.0"}
+                        </Text>
+                        <AntDesign name="star" style={styles.ratingIcon} />
+                    </View>
+                </View>
+                <Text style={styles.price}>
+                    {product.price ? product.price : "Product Price"}$
+                </Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: 200,
+        height: 200,
+        borderRadius: 20,
+        backgroundColor: "#6C4AB6",
+    },
+    productImage: {
+        width: "100%",
+        height: 140,
+        resizeMode: "cover",
+        alignSelf: "center",
+    },
+    tag: {
+        backgroundColor: "#FCF7FF",
+        color: "#8D72E1",
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderRadius: 20,
+        alignSelf: "flex-start",
+        marginTop: 10,
+        marginLeft: 5,
+    },
+    cartBtn: {
+        backgroundColor: "#FCF7FF",
+        height: 40,
+        width: 40,
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        bottom: 10,
+        right: 10,
+    },
+    cartIcon: {
+        fontSize: 20,
+        color: "#8D72E1",
+    },
+    wrapper: {
+        height: "100%",
+        paddingHorizontal: 10,
+    },
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    rating: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    ratingText: {
+        color: "#B9E0FF",
+        fontSize: 16,
+        marginRight: 2,
+    },
+    ratingIcon: {
+        color: "#B9E0FF",
+        fontSize: 18,
+        marginRight: 2,
+    },
+    title: {
+        color: "#B9E0FF",
+        fontSize: 16,
+    },
+    priceContainer: {
+        flexDirection: "row",
+    },
+    price: {
+        color: "#B9E0FF",
+        fontSize: 16,
+    },
+});
+
+export default Card;
