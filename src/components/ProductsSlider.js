b81dd632350d44eefe2e51bf677ec8db136c9fb7
KawshikBss <kawshikbss@gmail.com>
2023-01-16 03:27:01 +0600

    renamed to slider

---
+++ src/components/ProductsSlider.js
@@ -0,0 +1,63 @@
+import {
+    FlatList,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
+import Card from "./Card";
+
+const ProductsSlider = ({
+    header,
+    subHeader,
+    next,
+    items,
+    navigation,
+    vary = false,
+}) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>{header ? header : "Header"}</Text>
+            <ScrollView
+                horizontal
+                style={styles.itemsContainer}
+                showsHorizontalScrollIndicator={false}
+                decelerationRate={0}
+                snapToInterval={210}
+                snapToAlignment={'center'}
+            >
+                {items ? (
+                    items.map((item, index) => {
+                        return (
+                            <Card
+                                key={item.id}
+                                product={item}
+                            />
+                        );
+                    })
+                ) : (
+                    <Text>No Products</Text>
+                )}
+            </ScrollView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 20,
+    },
+    header: {
+        color: "#6C4AB6",
+        fontSize: 24,
+        fontWeight: "600",
+        marginBottom: 10,
+        marginLeft: 18,
+    },
+    itemsContainer: {
+        marginLeft: 16,
+    },
+});
+
+export default ProductsSlider;
