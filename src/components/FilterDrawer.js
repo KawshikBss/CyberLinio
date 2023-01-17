5513e13cd664d809af9279c68d9132c4ecd1c453
KawshikBss <kawshikbss@gmail.com>
2023-01-18 02:41:15 +0600

    added rawer sections

--- src/components/FilterDrawer.js
+++ src/components/FilterDrawer.js
@@ -4,14 +4,19 @@ import {
     DrawerLayoutAndroid,
     Modal,
     StyleSheet,
+    TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import FilterDrawerSection from "./FilterDrawerSection";
+import catagories from "../catagories";
+import colors from "../colors";
 
-const FilterDrawer = ({visible}) => {
+const FilterDrawer = ({visible, toggle}) => {
     return (
-        <Modal statusBarTranslucent transparent visible={visible}>
+        <Modal statusBarTranslucent transparent animationType="fade" visible={visible} onRequestClose={toggle}>
             <View style={styles.container}>
-                <Text>Filters</Text>
+                <FilterDrawerSection heading={'Catagories'} items={catagories} />
+                <FilterDrawerSection heading={'Color Family'} items={colors} />
             </View>
         </Modal>
     );
@@ -22,7 +27,12 @@ const styles = StyleSheet.create({
         width: "60%",
         height: '100%',
         alignSelf: 'flex-end',
-        backgroundColor: '#FCF7FF'
+        backgroundColor: '#FCF7FF',
+        borderTopLeftRadius: 20,
+        borderBottomLeftRadius: 20,
+        padding: 20,
+        borderLeftWidth: 2,
+        borderLeftColor: '#6C4AB6',
     },
 });
 
