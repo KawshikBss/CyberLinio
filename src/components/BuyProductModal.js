0cc1cb015339e2fc1b6b74b1150ed5eac89692d2
KawshikBss <kawshikbss@gmail.com>
2023-01-19 19:40:58 +0600

    fixed

--- src/components/BuyProductModal.js
+++ src/components/BuyProductModal.js
@@ -1,59 +1,81 @@
-import {
-    View,
-    Text,
-    Modal,
-    StyleSheet,
-    TouchableOpacity,
-    Image,
-} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import Modal from "react-native-modal";
 
 const BuyProductModal = ({ visible, toggle, product, amount, variant }) => {
     const navigation = useNavigation();
     return (
         <Modal
-            visible={visible}
+            isVisible={visible}
+            animationIn={"zoomIn"}
+            animationOut={"zoomOut"}
+            onBackdropPress={toggle}
             onRequestClose={toggle}
-            transparent={true}
-            animationType={"slide"}
+            statusBarTranslucent
+            deviceHeight={"100%"}
         >
             <View style={styles.container}>
-                <Image style={styles.image} source={product.images[0]} />
+                {/* <Image style={styles.image} source={product.images[0]} /> */}
                 <Text style={styles.title}>
                     {product?.title ? product.title : "Product Title"}
                 </Text>
                 <View style={styles.info}>
-                  <View style={styles.infoWrapper}>
-                    <Text style={styles.infoText}>Amount X </Text>
-                    <Text style={styles.infoText}>
-                        {amount ? amount : "0"}
-                    </Text>
+                    <View style={styles.infoWrapper}>
+                        <Text style={styles.infoText}>Amount X </Text>
+                        <Text style={styles.infoText}>
+                            {amount ? amount : "0"}
+                        </Text>
                     </View>
                     <Text style={styles.infoText}>
                         $ {product?.price ? product.price * amount : "0"}
                     </Text>
                 </View>
                 <View style={styles.info}>
-                  <View style={styles.infoWrapper}>
-                    <Text style={styles.infoText}>Selected Variant: </Text>
-                    <View
-                        style={StyleSheet.flatten([
-                            styles.variant,
-                            { backgroundColor: product?.variants? product.variants[variant]: '#FFF' },
-                        ])}
-                    />
+                    <View style={styles.infoWrapper}>
+                        <Text style={styles.infoText}>Selected Variant: </Text>
+                        <View
+                            style={StyleSheet.flatten([
+                                styles.variant,
+                                {
+                                    backgroundColor: product?.variants
+                                        ? product.variants[variant]
+                                        : "#FFF",
+                                },
+                            ])}
+                        />
                     </View>
                 </View>
                 <View style={styles.info}>
-                <TouchableOpacity onPress={toggle} style={StyleSheet.flatten([styles.btn, {backgroundColor: '#8D9EFF'}])}>
-                    <Text style={styles.btnText}>Buy Now</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => {toggle(); navigation.navigate('HomeTab', {screen: 'Cart'})}} style={StyleSheet.flatten([styles.btn, {backgroundColor: '#8D9EFF'}])}>
-                    <Text style={styles.btnText}>Go To Cart</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={toggle}
+                        style={StyleSheet.flatten([
+                            styles.btn,
+                            { backgroundColor: "#8D9EFF" },
+                        ])}
+                    >
+                        <Text style={styles.btnText}>Buy Now</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            toggle();
+                            navigation.navigate("HomeTab", { screen: "Cart" });
+                        }}
+                        style={StyleSheet.flatten([
+                            styles.btn,
+                            { backgroundColor: "#8D9EFF" },
+                        ])}
+                    >
+                        <Text style={styles.btnText}>Go To Cart</Text>
+                    </TouchableOpacity>
                 </View>
-                <TouchableOpacity onPress={toggle} style={StyleSheet.flatten([styles.btn, {backgroundColor: '#6C4AB6'}])}>
+                <TouchableOpacity
+                    onPress={toggle}
+                    style={StyleSheet.flatten([
+                        styles.btn,
+                        { backgroundColor: "#B9E0FF" },
+                    ])}
+                >
                     <Text style={styles.btnText}>Keep Shopping</Text>
                 </TouchableOpacity>
             </View>
@@ -63,17 +85,18 @@ const BuyProductModal = ({ visible, toggle, product, amount, variant }) => {
 
 const styles = StyleSheet.create({
     container: {
-        height: "70%",
-        width: "80%",
-        backgroundColor: "#8D72E1",
+        height: "45%",
+        width: "90%",
+        backgroundColor: "#6C4AB6",
         borderRadius: 50,
-        borderBottomWidth: 5,
+        // borderBottomWidth: 2,
         borderRightWidth: 5,
         borderColor: "#8D9EFF",
         marginTop: 100,
         justifyContent: "space-evenly",
         alignItems: "center",
         alignSelf: "center",
+        position: "absolute",
     },
     image: {
         borderRadius: 20,
@@ -94,7 +117,9 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
     },
     infoWrapper: {
-      flexDirection: 'row', alignItems: 'center', justifyContent: 'center',
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
     },
     infoText: {
         // fontSize: 16,
@@ -105,18 +130,18 @@ const styles = StyleSheet.create({
         height: 24,
         borderRadius: 50,
         borderWidth: 4,
-        borderColor: '#B9E0FF',
+        borderColor: "#B9E0FF",
     },
     btn: {
-      paddingHorizontal: 15,
-      paddingVertical: 5,
-      borderRadius: 20,
-      marginVertical: 10,
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        borderRadius: 20,
+        marginVertical: 10,
     },
     btnText: {
-      fontSize: 16,
-      color: "#B9E0FF",
-    }
+        fontSize: 16,
+        color: "#6C4AB6",
+    },
 });
 
 export default BuyProductModal;
