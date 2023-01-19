547c4ac2da8c2924a6e800ae112c3120088c1144
KawshikBss <kawshikbss@gmail.com>
2023-01-19 19:41:30 +0600

    added bottom margin

--- src/components/FilterDrawerSection.js
+++ src/components/FilterDrawerSection.js
@@ -1,11 +1,17 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    TextInput,
+} from "react-native";
 import React, { useState } from "react";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-const FilterDrawerSection = ({ heading, items }) => {
+const FilterDrawerSection = ({ heading, items, range }) => {
     const [extended, setExtended] = useState(false);
     return (
-        <View>
+        <View style={styles.container}>
             <View style={styles.section}>
                 <Text style={styles.heading}>
                     {heading ? heading : "Heading"}
@@ -18,7 +24,7 @@ const FilterDrawerSection = ({ heading, items }) => {
                         })
                     }
                 >
-                    {extended ? (
+                    {range? '': (extended ? (
                         <FontAwesome
                             name="angle-up"
                             style={styles.extendBtnIcon}
@@ -28,10 +34,16 @@ const FilterDrawerSection = ({ heading, items }) => {
                             name="angle-down"
                             style={styles.extendBtnIcon}
                         />
-                    )}
+                    ))}
                 </TouchableOpacity>
             </View>
-            {extended ? (
+            {range ? (
+                <View style={styles.rangeContainer}>
+                    <TextInput style={styles.rangeInput} placeholder='MIN' />
+                    <Text style={{color: '#6C4AB6', fontSize: 25}}>-</Text>
+                    <TextInput style={styles.rangeInput} placeholder='MAX' />
+                </View>
+            ) : extended ? (
                 <View style={styles.itemsContainer}>
                     {items
                         ? items.map((item, index) => {
@@ -50,7 +62,10 @@ const FilterDrawerSection = ({ heading, items }) => {
     );
 };
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
+    container: {
+        marginBottom: 10,
+    },
     section: {
         flexDirection: "row",
         justifyContent: "space-between",
@@ -86,6 +101,22 @@ const styles = StyleSheet.create({
     itemsContainer: {
         flexWrap: "wrap",
         flexDirection: "row",
+    },
+    rangeContainer: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+    },
+    rangeInput: {
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        width: 70,
+        height: 30,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: "#6C4AB6",
+        marginTop: 10,
+        color: "#6C4AB6",
     },
 });
 
