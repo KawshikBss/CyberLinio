ec83c5e54c22ff66660a2e670610237a19a3e2f6
KawshikBss <kawshikbss@gmail.com>
2023-01-21 01:37:36 +0600

    for users

---
+++ src/users.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+        id: 0,
+        name: 'Elon Musk',
+        image: require('../assets/User.webp'),
+        phone: '+880-17-8359-4455',
+        email: 'emusk@tesla.com',
+        address: 'Musk Land, Mars',
+    }
+];
+
+export default users;
