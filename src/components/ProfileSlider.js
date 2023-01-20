361aee1a8020683074ec78857db6a144c1d87dfd
KawshikBss <kawshikbss@gmail.com>
2023-01-21 01:38:46 +0600

    created profile slider to display sections

---
+++ src/components/ProfileSlider.js
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import React from 'react'
+import ProfileSection from './ProfileSection';
+import profileSections from '../profileSections';
+
+const ProfileSlider = () => {
+  return (
+    <ScrollView horizontal contentContainerStyle={styles.container} snapToInterval={400}>
+        {
+            profileSections.map((section, index) => {
+                return (
+                    <ProfileSection key={index} section={section} />
+                );
+            })
+        }
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 2,
+    marginBottom: 100,
+  }
+});
+
+export default ProfileSlider
