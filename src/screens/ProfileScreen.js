8036c351042cf013bdd46329c47b5cb6d5b6d340
KawshikBss <kawshikbss@gmail.com>
2023-01-21 02:28:47 +0600

    added extending funtionality to userInfo

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,24 +1,30 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
-import React from 'react'
-import ProfileUser from '../components/ProfileUser';
-import ProfileSection from '../components/ProfileSection';
-import ProfileSlider from '../components/ProfileSlider';
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import ProfileUser from "../components/ProfileUser";
+import ProfileSection from "../components/ProfileSection";
+import ProfileSlider from "../components/ProfileSlider";
 
 const ProfileScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ProfileUser />
-      <ProfileSlider />
-    </SafeAreaView>
-  )
-}
+    const [extended, setExtended] = useState(false);
+    const toggle = () => {
+        setExtended((curr) => {
+            return !curr;
+        });
+    };
+    return (
+        <SafeAreaView style={styles.container}>
+            <ProfileUser extended={extended} toggle={toggle} />
+            {!extended ? <ProfileSlider /> : ""}
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-      height: "100%",
-      backgroundColor: "#8D72E1",
-      flexDirection: "column",
-  },
+    container: {
+        height: "100%",
+        backgroundColor: "#8D72E1",
+        flexDirection: "column",
+    },
 });
 
-export default ProfileScreen
+export default ProfileScreen;
