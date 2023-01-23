e84188c17c7578d595fd9885f3a0be97fb2cccf2
KawshikBss <kawshikbss@gmail.com>
2023-01-23 14:57:13 +0600

    added pagination

--- src/components/ProfileSlider.js
+++ src/components/ProfileSlider.js
@@ -1,11 +1,20 @@
 import { View, Text, StyleSheet, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import ProfileSection from './ProfileSection';
 import profileSections from '../profileSections';
+import Pagination from './Pagination';
 
 const ProfileSlider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const handleScroll = event => {
+    const xOffset = event.nativeEvent.contentOffset.x;
+    const currentSelect = Math.ceil((xOffset / 400));
+    setCurrentSlide(currentSelect);
+    console.log(xOffset);
+  }
   return (
-    <ScrollView horizontal contentContainerStyle={styles.container} snapToInterval={400}>
+    <View style={styles.container}>
+    <ScrollView horizontal snapToInterval={400} showsHorizontalScrollIndicator={false} pagingEnabled={true} onScroll={handleScroll}>
         {
             profileSections.map((section, index) => {
                 return (
@@ -14,12 +23,14 @@ const ProfileSlider = () => {
             })
         }
     </ScrollView>
+    <Pagination items={profileSections} selected={currentSlide} />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 2,
+    flex: 2,
     marginBottom: 90,
   }
 });
