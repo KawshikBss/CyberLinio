a79bde66594985df75afe3c3233c0168f70995c4
KawshikBss <kawshikbss@gmail.com>
2023-01-24 02:48:17 +0600

    for storing product reviews

---
+++ src/reviews.js
@@ -0,0 +1,28 @@
+const reviews = [
+ {
+    id: 0,
+    username: 'Irure eiusmod',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    rating: 2,
+ },
+ {
+    id: 1,
+    username: 'sadasdas eiusmod',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    rating: 5,
+ },
+ {
+    id: 2,
+    username: 'usasdasmod',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    rating: 1,
+ },
+ {
+    id: 3,
+    username: 'eiusmod Irure',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    rating: 6,
+ },
+];
+
+export default reviews;
