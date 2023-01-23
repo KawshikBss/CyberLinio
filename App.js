8fe0c9e881b612b5851142c5ec849fb7e7a5c68d
KawshikBss <kawshikbss@gmail.com>
2023-01-23 23:28:42 +0600

    added login and signup screens

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import ShopScreen from "./src/screens/ShopScreen";
 import CartScreen from "./src/screens/CartScreen";
 import ProfileScreen from "./src/screens/ProfileScreen";
 import WishScreen from "./src/screens/WishScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import SignupScreen from "./src/screens/SignupScreen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -80,6 +82,8 @@ export default function App() {
                 <Stack.Screen name="Splash" component={SplashScreen} />
                 <Stack.Screen name="HomeTab" component={HomeTab} />
                 <Stack.Screen name="Product" component={ProductScreen} />
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Signup" component={SignupScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
