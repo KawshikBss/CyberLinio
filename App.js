15c83215404c564cbfc9427c02514f7e0b70f02c
KawshikBss <kawshikbss@gmail.com>
2023-01-25 22:39:26 +0600

    added new orders stack

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import ProductScreen from "./src/screens/ProductScreen";
 import SplashScreen from "./src/screens/SplashScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import SignupScreen from "./src/screens/SignupScreen";
-import OrdersScreen from "./src/screens/OrdersScreen";
+import OrdersScreen from "./src/screens/AllOrdersScreen";
 import ShopStack from "./src/screens/ShopStack";
 import OrdersStack from "./src/screens/OrdersStack";
 
