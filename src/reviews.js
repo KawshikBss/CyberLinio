6d39d4e8fd4c7caafbe3f7fa45eae25752519007
KawshikBss <kawshikbss@gmail.com>
2023-01-25 22:39:57 +0600

    updated user reviews

--- src/reviews.js
+++ src/reviews.js
@@ -4,24 +4,28 @@ const reviews = [
     username: 'Irure eiusmod',
     desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
     rating: 2,
+    user_image: require('../assets/User.webp'),
  },
  {
     id: 1,
     username: 'sadasdas eiusmod',
     desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
     rating: 5,
+    user_image: require('../assets/Sweater2.jpg'),
  },
  {
     id: 2,
     username: 'usasdasmod',
     desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
     rating: 1,
+    user_image: require('../assets/Sweater3.jpg'),
  },
  {
     id: 3,
     username: 'eiusmod Irure',
     desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
     rating: 6,
+    user_image: require('../assets/Sweater.jpg'),
  },
 ];
 
