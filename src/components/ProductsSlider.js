f3686db28f1d14fcd96cd0caaeb185606df56b7d
KawshikBss <kawshikbss@gmail.com>
2023-01-26 14:40:46 +0600

    added se all button

--- src/components/ProductsSlider.js
+++ src/components/ProductsSlider.js
@@ -8,16 +8,15 @@ import {
     View,
 } from "react-native";
 import Card from "./Card";
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { useNavigation } from "@react-navigation/native";
 
 const ProductsSlider = ({
     header,
-    subHeader,
-    next,
     items,
-    navigation,
-    vary = false,
     finalSection = false
 }) => {
+    const navigation = useNavigation();
     const [currentItem, setCurrentItem] = useState(0);
     const handleScroll = event => {
       const xOffset = event.nativeEvent.contentOffset.x;
@@ -25,7 +24,6 @@ const ProductsSlider = ({
       if (currentSelect > items.length - 1) currentSelect -= 1;
       setCurrentItem(currentSelect);
     }
-    console.log(currentItem);
     return (
         <View style={StyleSheet.flatten([styles.container, {marginBottom: finalSection? styles.finalContainer.marginBottom: styles.container.marginBottom}])}>
             <Text style={styles.header}>{header ? header : "Header"}</Text>
@@ -51,6 +49,12 @@ const ProductsSlider = ({
                 ) : (
                     <Text>No Products</Text>
                 )}
+                    <View style={styles.allBtnWrapper}>
+                        <TouchableOpacity style={styles.allBtn} onPress={() => navigation.navigate('ShopStack', {screen: 'Shop'})}>
+                            <Text style={styles.allBtnText}>See All </Text>
+                            <AntDesign name="arrowright" style={styles.allBtnIcon} />
+                        </TouchableOpacity>
+                    </View>
             </ScrollView>
         </View>
     );
@@ -72,6 +76,33 @@ const styles = StyleSheet.create({
     },
     itemsContainer: {
         marginLeft: 16,
+    },
+    allBtnWrapper: {
+        height: 200,
+        width: 140,
+        marginHorizontal: 10,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+    },
+    allBtn: {
+        height: 100,
+        width: 90,
+        borderRadius: 100,
+        borderRightWidth: 5,
+        borderBottomWidth: 5,
+        borderColor: '#6C4AB6',
+        backgroundColor: '#FCF7FF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        opacity: 0.8
+    },
+    allBtnText: {
+        color: '#6C4AB6',
+        fontWeight: '700',
+    },
+    allBtnIcon: {
+        color: '#6C4AB6',
+        fontSize: 30,
     },
 });
 
