8ac69893561d3ba7fe56248a04b1a7be611cbd42
KawshikBss <kawshikbss@gmail.com>
2023-01-26 17:56:10 +0600

    for coupons

---
+++ src/coupons.js
@@ -0,0 +1,39 @@
+const coupons = [
+    {
+        id: 0,
+        code: 'Hello1',
+        validity: '20-12-2023'
+    },
+    {
+        id: 1,
+        code: 'Hello1',
+        validity: '20-12-2023'
+    },
+    {
+        id: 2,
+        code: 'Hello2',
+        validity: '20-12-2023'
+    },
+    {
+        id: 3,
+        code: 'Hello3',
+        validity: '20-12-2023'
+    },
+    {
+        id: 4,
+        code: 'Hello4',
+        validity: '20-12-2023'
+    },
+    {
+        id: 5,
+        code: 'Hello5',
+        validity: '20-12-2023'
+    },
+    {
+        id: 6,
+        code: 'Hello6',
+        validity: '20-12-2023'
+    },
+];
+
+export default coupons;
