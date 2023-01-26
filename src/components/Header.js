dd4b5b66ce015acfb4bc23419a92e42c072e566c
KawshikBss <kawshikbss@gmail.com>
2023-01-26 14:39:06 +0600

    fixed notifications link

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,23 +8,31 @@ import {
 import React from "react";
 import Feather from "react-native-vector-icons/Feather";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
 
 const Header = () => {
+    const navigation = useNavigation();
     return (
         <View style={styles.container}>
             <View style={styles.searchWrapper}>
                 <TouchableOpacity style={styles.searchBtn}>
                     <Feather name="search" style={styles.searchBtnText} />
                 </TouchableOpacity>
-                <TextInput style={styles.searchInput} placeholder={'What are you looking for?'} />
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder={"What are you looking for?"}
+                />
             </View>
             <View style={styles.notificationBtnWrapper}>
-            <TouchableOpacity style={styles.notificationBtn}>
-                <Ionicons
-                    name="notifications-outline"
-                    style={styles.notificationBtnText}
-                />
-            </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.notificationBtn}
+                    onPress={() => navigation.navigate("Notifications")}
+                >
+                    <Ionicons
+                        name="notifications-outline"
+                        style={styles.notificationBtnText}
+                    />
+                </TouchableOpacity>
             </View>
         </View>
     );
@@ -32,53 +40,53 @@ const Header = () => {
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '100%',
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
         marginHorizontal: 8,
         marginTop: 16,
         marginBottom: 7,
     },
     searchWrapper: {
         flex: 4,
-        flexDirection: 'row',
-        alignItems: 'center',
+        flexDirection: "row",
+        alignItems: "center",
         padding: 24,
-        backgroundColor: '#FCF7FF',
+        backgroundColor: "#FCF7FF",
         borderRadius: 50,
     },
     searchInput: {
-        width: '80%',
-        position: 'absolute',
+        width: "80%",
+        position: "absolute",
         left: 60,
     },
     searchBtn: {
-        position: 'absolute',
+        position: "absolute",
         left: 11,
-        alignSelf: 'center',
+        alignSelf: "center",
         zIndex: 1,
     },
     searchBtnText: {
-        color: '#6C4AB6',
+        color: "#6C4AB6",
         fontSize: 28,
     },
     notificationBtnWrapper: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
     },
     notificationBtn: {
-        backgroundColor: '#FCF7FF',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#FCF7FF",
+        alignItems: "center",
+        justifyContent: "center",
         width: 46,
         height: 46,
         borderRadius: 50,
         marginRight: 8,
     },
     notificationBtnText: {
-        color: '#6C4AB6',
+        color: "#6C4AB6",
         fontSize: 28,
     },
 });
