a463a665c12201c342a36802046fa65ee6692b90
KawshikBss <kawshikbss@gmail.com>
2023-01-26 14:37:49 +0600

    for messages

---
+++ src/messages.js
@@ -0,0 +1,52 @@
+const messages = [
+ {
+    id: 0,
+    username: 'Irure eiusmod',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    user_image: require('../assets/User.webp'),
+ },
+ {
+    id: 1,
+    username: 'sadasdas eiusmod',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    user_image: require('../assets/Sweater2.jpg'),
+ },
+ {
+    id: 2,
+    username: 'usasdasmod',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    user_image: require('../assets/Sweater3.jpg'),
+ },
+ {
+    id: 3,
+    username: 'eiusmod Irure',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    user_image: require('../assets/Sweater.jpg'),
+ },
+ {
+    id: 4,
+    username: 'Irure eiusmod',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    user_image: require('../assets/User.webp'),
+ },
+ {
+    id: 5,
+    username: 'sadasdas eiusmod',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    user_image: require('../assets/Sweater2.jpg'),
+ },
+ {
+    id: 6,
+    username: 'usasdasmod',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    user_image: require('../assets/Sweater3.jpg'),
+ },
+ {
+    id: 7,
+    username: 'eiusmod Irure',
+    desc: 'Ea cupidatat dolore nulla tempor veniam. Enim amet incididunt quis incididunt excepteur sunt.',
+    user_image: require('../assets/Sweater.jpg'),
+ },
+];
+
+export default messages;
