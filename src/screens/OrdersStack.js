a1af059b874c886e1320cbad92acd1eed43bcd3a
KawshikBss <kawshikbss@gmail.com>
2023-01-30 22:00:18 +0600

    changed material top tab to bottom tab

--- src/screens/OrdersStack.js
+++ src/screens/OrdersStack.js
@@ -1,12 +1,14 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React, { lazy } from 'react'
 import OrdersScreen from './AllOrdersScreen';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import PendingOrdersScreen from './PendingOrdersScreen';
 import CancelledOrdersScreen from './CancelledOrdersScreen';
 import DeliveredOrdersScreen from './DeliveredOrdersScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Tab = createMaterialTopTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const OrdersStack = () => {
   return (
@@ -15,6 +17,27 @@ const OrdersStack = () => {
             tabBarStyle: { ...styles.navBar },
             headerShown: false,
             tabBarLabelStyle: {...styles.tabLabel},
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName = "all-inclusive-box";
+                if (route.name === "Pending") {
+                    iconName = "timer-sand";
+                } else if (route.name === "Cancelled") {
+                    iconName = "cancel";
+                } else if (route.name === "Delivered") {
+                    iconName = "truck-delivery";
+                }
+                return (
+                        <MaterialCommunityIcons
+                            name={iconName}
+                            color={color}
+                            size={focused ? 30 : 24}
+                            style={{
+                                height: 30,
+                                width: 30
+                            }}
+                        />
+                );
+            },
             tabBarInactiveTintColor: "#8D72E1",
             tabBarActiveTintColor: "#B9E0FF",
         })}
@@ -29,19 +52,20 @@ const OrdersStack = () => {
 
 const styles = StyleSheet.create({
     navBar: {
-        height: 60,
-        backgroundColor: '#6C4AB6'
+        height: 80,
+        backgroundColor: '#6C4AB6',
+        padding: 10,
     },
     tabActiveInner: {
         backgroundColor: "#8D72E133",
-        flexDirection: "row",
-        borderWidth: 1,
+        // flexDirection: "row",
     },
     tabLabel: {
         // color: "#8D72E1",
         fontWeight: "bold",
         fontSize: 10,
-        alignSelf: 'center'
+        alignSelf: 'center',
+        marginBottom: 10,
     },
 });
 
