0f600b275d0e8d746a47962c5004a24cfcc2e8ac
KawshikBss <kawshikbss@gmail.com>
2023-02-03 18:36:11 +0600

    fixed dynamic content

--- src/components/ProductsSlider.js
+++ src/components/ProductsSlider.js
@@ -8,54 +8,79 @@ import {
     View,
 } from "react-native";
 import Card from "./Card";
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import AntDesign from "react-native-vector-icons/AntDesign";
 import { useNavigation } from "@react-navigation/native";
 
 const ProductsSlider = ({
     header,
     items,
-    finalSection = false
+    isLoading = true,
+    isSuccess = false,
+    finalSection = false,
 }) => {
     const navigation = useNavigation();
     const [currentItem, setCurrentItem] = useState(0);
-    const handleScroll = event => {
-      const xOffset = event.nativeEvent.contentOffset.x;
-      let currentSelect = Math.ceil((xOffset / 230));
-      if (currentSelect > items.length - 1) currentSelect -= 1;
-      setCurrentItem(currentSelect);
-    }
+    const handleScroll = (event) => {
+        const xOffset = event.nativeEvent.contentOffset.x;
+        let currentSelect = Math.ceil(xOffset / 230);
+        if (currentSelect > items.length - 1) currentSelect -= 1;
+        setCurrentItem(currentSelect);
+    };
     return (
-        <View style={StyleSheet.flatten([styles.container, {marginBottom: finalSection? styles.finalContainer.marginBottom: styles.container.marginBottom}])}>
+        <View
+            style={StyleSheet.flatten([
+                styles.container,
+                {
+                    marginBottom: finalSection
+                        ? styles.finalContainer.marginBottom
+                        : styles.container.marginBottom,
+                },
+            ])}
+        >
             <Text style={styles.header}>{header ? header : "Header"}</Text>
-            <ScrollView
-                horizontal
-                style={styles.itemsContainer}
-                showsHorizontalScrollIndicator={false}
-                decelerationRate={0}
-                snapToInterval={230}
-                snapToAlignment={'center'}
-                onScroll={handleScroll}
-            >
-                {items ? (
-                    items.map((item, index) => {
-                        return (
-                            <Card
-                                key={item.id}
-                                product={item}
-                                focused={index === currentItem}
-                            />
-                        );
-                    })
-                ) : (
-                    <Text>No Products</Text>
-                )}
+            {isLoading ? (
+                <Text>Loading...</Text>
+            ) : (
+                <ScrollView
+                    horizontal
+                    style={styles.itemsContainer}
+                    showsHorizontalScrollIndicator={false}
+                    decelerationRate={0}
+                    snapToInterval={230}
+                    snapToAlignment={"center"}
+                    onScroll={handleScroll}
+                >
+                    {isSuccess && items ? (
+                        items.map((item, index) => {
+                            return (
+                                <Card
+                                    key={item.id}
+                                    product={item}
+                                    focused={index === currentItem}
+                                />
+                            );
+                        })
+                    ) : (
+                        <Text>No Products</Text>
+                    )}
                     <View style={styles.allBtnWrapper}>
-                        <TouchableOpacity style={styles.allBtn} onPress={() => navigation.navigate('ShopStack', {screen: 'Shop'})}>
+                        <TouchableOpacity
+                            style={styles.allBtn}
+                            onPress={() =>
+                                navigation.navigate("ShopStack", {
+                                    screen: "Shop",
+                                })
+                            }
+                        >
                             <Text style={styles.allBtnText}>See All </Text>
-                            <AntDesign name="arrowright" style={styles.allBtnIcon} />
+                            <AntDesign
+                                name="arrowright"
+                                style={styles.allBtnIcon}
+                            />
                         </TouchableOpacity>
                     </View>
-            </ScrollView>
+                </ScrollView>
+            )}
         </View>
     );
 };
@@ -81,8 +106,8 @@ const styles = StyleSheet.create({
         height: 200,
         width: 140,
         marginHorizontal: 10,
-        alignItems: 'flex-start',
-        justifyContent: 'center',
+        alignItems: "flex-start",
+        justifyContent: "center",
     },
     allBtn: {
         height: 100,
@@ -90,18 +115,18 @@ const styles = StyleSheet.create({
         borderRadius: 100,
         borderRightWidth: 5,
         borderBottomWidth: 5,
-        borderColor: '#6C4AB6',
-        backgroundColor: '#FCF7FF',
-        alignItems: 'center',
-        justifyContent: 'center',
-        opacity: 0.8
+        borderColor: "#6C4AB6",
+        backgroundColor: "#FCF7FF",
+        alignItems: "center",
+        justifyContent: "center",
+        opacity: 0.8,
     },
     allBtnText: {
-        color: '#6C4AB6',
-        fontWeight: '700',
+        color: "#6C4AB6",
+        fontWeight: "700",
     },
     allBtnIcon: {
-        color: '#6C4AB6',
+        color: "#6C4AB6",
         fontSize: 30,
     },
 });
