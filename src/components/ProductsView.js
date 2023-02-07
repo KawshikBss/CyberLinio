644b8b58325ea0b81b685563210fa3c2bbba750a
KawshikBss <kawshikbss@gmail.com>
2023-02-07 22:27:18 +0600

    added skeleton

--- src/components/ProductsView.js
+++ src/components/ProductsView.js
@@ -1,43 +1,44 @@
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import React from "react";
 import Card from "./Card";
+import CardSkeleton from "./CardSkeleton";
 
-const ProductsView = ({ products }) => {
+const ProductsView = ({ isSuccess, isLoading, products }) => {
     return (
         <View style={styles.container}>
-        <ScrollView >
-            <View style={styles.wrapper}>
-            {products ? (
-                products.map((product, index) => {
-                    return <Card key={index} product={product} shopView />;
-                })
-            ) : (
-                <Text>No Products</Text>
-            )}
-            {products ? (
-                products.map((product, index) => {
-                    return <Card key={index} product={product} shopView />;
-                })
-            ) : (
-                <Text>No Products</Text>
-            )}
-            </View>
-        </ScrollView></View>
+            <ScrollView>
+                <View style={styles.wrapper}>
+                    {isSuccess && products ? (
+                        products.map((product, index) => {
+                            return (
+                                <Card key={index} product={product} shopView />
+                            );
+                        })
+                    ) : isLoading ? (
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((item, index) => {
+                            return <CardSkeleton key={index} shopView />;
+                        })
+                    ) : (
+                        <Text>No Products</Text>
+                    )}
+                </View>
+            </ScrollView>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
         paddingBottom: 270,
     },
     wrapper: {
-        flexWrap: 'wrap',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+        flexWrap: "wrap",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
 
 export default ProductsView;
