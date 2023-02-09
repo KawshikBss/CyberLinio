72a24f49d1d07891a70045497d27d9d2e1d3ff61
KawshikBss <kawshikbss@gmail.com>
2023-02-10 01:51:23 +0600

    fixed dynamic content

--- src/components/Review.js
+++ src/components/Review.js
@@ -7,14 +7,18 @@ const Review = ({ item }) => {
         <View style={styles.container}>
             <View style={styles.userWrapper}>
                 <Image
-                    source={item.user_image}
+                    source={require(`../../assets/User.webp`)}
                     style={styles.userImg}
                 />
-                <Text style={styles.userName}>{item.username}</Text>
+                <Text style={styles.userName}>
+                    {item?.reviewer ? item.reviewer : "N/A"}
+                </Text>
             </View>
-            <Text style={styles.userName}>{'>'}</Text>
+            <Text style={styles.userName}>{">"}</Text>
             <View style={styles.wrapper}>
-                <Text style={styles.reviewDesc}>{item.desc}</Text>
+                <Text style={styles.reviewDesc}>
+                    {item?.review ? item.review : "N/A"}
+                </Text>
                 <View style={styles.ratingContainer}>
                     <Text style={styles.ratingText}>{item.rating}</Text>
                     <AntDesign name="star" style={styles.ratingIcon} />
@@ -26,16 +30,16 @@ const Review = ({ item }) => {
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
-        justifyContent:'space-between',
+        flexDirection: "row",
+        justifyContent: "space-between",
         marginVertical: 10,
     },
     userWrapper: {
-        width: '30%',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
+        width: "30%",
+        justifyContent: "space-evenly",
+        alignItems: "center",
         borderRightWidth: 1,
-        borderColor: '#8D9EFF',
+        borderColor: "#8D9EFF",
         padding: 5,
     },
     userImg: {
@@ -44,29 +48,30 @@ const styles = StyleSheet.create({
         borderRadius: 10,
     },
     userName: {
-        color: '#8D9EFF',
+        color: "#8D9EFF",
+        fontSize: 12,
     },
     wrapper: {
-        width: '60%',
-        justifyContent: 'space-evenly',
-        alignItems: 'flex-start',
+        width: "60%",
+        justifyContent: "space-evenly",
+        alignItems: "flex-start",
         marginLeft: 5,
     },
     reviewDesc: {
         fontSize: 12,
-        color: '#6C4AB6',
+        color: "#6C4AB6",
     },
     ratingContainer: {
-        flexDirection: 'row',
-        justifyContent:'space-between',
-        alignItems: 'center',
-        alignSelf: 'flex-end',
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        alignSelf: "flex-end",
     },
     ratingIcon: {
-        color: '#8D9EFF',
+        color: "#8D9EFF",
     },
     ratingText: {
-        color: '#8D9EFF',
+        color: "#8D9EFF",
     },
 });
 
